e616ec216fab7781df2092c5cc2bc082ce08aafb
meanwhile131 <51206659+meanwhile131@users.noreply.github.com>
2025-12-19 09:51:14 +0400

    Add navigation landmarks

--- index.php
+++ index.php
@@ -10,32 +10,36 @@
 </head>
 
 <body>
-    <h3>Шкала оценки зависимости от персонального компьютера, интернета и мобильных устройств, обеспечивающих доступ к
-        нему
-    </h3>
-    <div id="form_container">
-        <form action="/results.php" method="post" autocomplete="off">
-            <?php
-            include "data.php";
-            $keys = array_keys($questions);
-            shuffle($keys);
-            foreach ($keys as $key) {
-                $questions_shuffled[$key] = $questions[$key];
-            }
-            foreach ($questions_shuffled as $id => $question) {
-                echo "<label class='question'>$question<br><div class='answer_container'>";
-                $category = explode('-', $id)[0];
-                foreach ($categories[$category][1] as $answer_id => list($answer_value, $answer_name)) {
-                    echo "<label class='answer'><input type='radio' name='$id' value='$answer_id' required>$answer_name</label>";
+    <header>
+        <h3>Шкала оценки зависимости от персонального компьютера, интернета и мобильных устройств, обеспечивающих доступ к
+            нему
+        </h3>
+    </header>
+    <main>
+        <div id="form_container">
+            <form action="/results.php" method="post" autocomplete="off">
+                <?php
+                include "data.php";
+                $keys = array_keys($questions);
+                shuffle($keys);
+                foreach ($keys as $key) {
+                    $questions_shuffled[$key] = $questions[$key];
                 }
-                echo "</div></label>";
-            }
-            ?>
-            <br>
-            <br>
-            <button type="submit">Отправить</button>
-        </form>
-    </div>
+                foreach ($questions_shuffled as $id => $question) {
+                    echo "<label class='question'>$question<br><div class='answer_container'>";
+                    $category = explode('-', $id)[0];
+                    foreach ($categories[$category][1] as $answer_id => list($answer_value, $answer_name)) {
+                        echo "<label class='answer'><input type='radio' name='$id' value='$answer_id' required>$answer_name</label>";
+                    }
+                    echo "</div></label>";
+                }
+                ?>
+                <br>
+                <br>
+                <button type="submit">Отправить</button>
+            </form>
+        </div>
+    </main>
 </body>
 
 </html>
